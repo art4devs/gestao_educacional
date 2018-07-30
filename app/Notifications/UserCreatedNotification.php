3d1b505eeb9dbd49f9445b7eb459f8d5253b7e4e
Danilo Tiago <programacao.desenvolvimento@gmail.com>
2018-07-29 21:26:23 -0300

    configurando envio de email de bodas vindas

---
+++ app/Notifications/UserCreatedNotification.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Educacional\Notifications;
+
+use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class UserCreatedNotification extends Notification
+{
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                ->subject("Seja bem vindo a EdUcA!")
+                ->salutation("Sua conta no EdUcA acaba de ser criada.")
+                ->line("Seu número de matricula é {$notifiable->enrolment}")
+                ->action('Notification Action', url('/'))
+                ->line("Obrigado por estar conosco em mais este novo ciclo!");
+    }
+}
--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -6,6 +6,10 @@
         @include('global_includes.form-errors-messages')
         {{ Form::open(['route' => 'admin.users.store']) }}
             @include('admin.users.includes._form')
+            <div class="form-group">
+                {{Form::label('send_mail', 'Enviar e-mail de boas vindas')}}
+                {{Form::checkbox('send_mail', '', true, ['class' => 'pull-left'])}}
+            </div>
             {{ Form::submit('Salvar', ['class' => 'btn btn-success']) }}
             {{ link_to_route('admin.users.index', 'Voltar', [], ['class' => 'btn btn-warning pull-right']) }}
         {{ Form::close() }}
